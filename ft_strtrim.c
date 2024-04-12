446a74ab664b332f0d4caf4599e768daf7494889
Aurelio Sideris <asideris@shi-c1-p3.s19.be>
2024-04-12 19:23:31 +0200

    s

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,25 +6,14 @@
 /*   By: asideris <asideris@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:43:04 by asideris          #+#    #+#             */
-/*   Updated: 2024/04/11 18:09:10 by asideris         ###   ########.fr       */
+/*   Updated: 2024/04/12 19:17:35 by asideris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
-int	ft_strlen(char const *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		i++;
-	}
-	return (i);
-}
-
-int	ft_isset(char c, char const *set)
+static int	ft_isset(char c, char const *set)
 {
 	int	i;
 
@@ -44,44 +33,30 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
 	int		j;
-	char	*dest;
-	int		setcount;
-	int		len;
+	int		start;
+	int		end;
 
-	len = ft_strlen(s1);
 	i = 0;
 	j = 0;
-	setcount = 0;
-	while (s1[i])
+	start = 0;
+	end = ft_strlen((char *)s1);
+
+	while (ft_isset(s1[start], set) == 1)
 	{
-		if (ft_isset(s1[i], set) == 1)
-		{
-			setcount++;
-		}
-		i++;
+		start++;
 	}
-	dest = malloc(sizeof(char) * (len - setcount));
-	i = 0;
-	while (dest[i])
+	while (ft_isset(s1[end - 1], set) == 1)
 	{
-		if (ft_isset(s1[i], set) == 0)
-		{
-			dest[j] = s1[i];
-			j++;
-		}
-		i++;
+		end--;
 	}
-	dest[i] = '\0';
-	return (dest);
+	if (end == start)
+		return (ft_strdup(""));
+	return (ft_substr(s1, start, end - start));
+
+
 }
 #include<stdio.h>
 int	main(void)
 {
-	char	*s1;
-	char	*set;
-
-	s1 = "hellotheresir";
-	set = "oil";
-	printf("%s",ft_strtrim(s1, set));
-	return(0);
+	printf("%s\n", ft_strtrim("baHELLOab", "ab"));
 }
--- libft.h
+++ libft.h
@@ -1,42 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asideris <asideris@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 14:52:38 by asideris          #+#    #+#             */
-/*   Updated: 2024/04/12 14:58:46 by asideris         ###   ########.fr       */
+/*   Created: 2024/04/11 17:43:04 by asideris          #+#    #+#             */
+/*   Updated: 2024/04/12 17:07:09 by asideris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_H
+# define LIBFT_H
+
 #include <stdlib.h>
-#include "libft.h"
+#include <stddef.h>
 
-char	*ft_strdup(const char *s1)
-{
-	char	*s2;
-	int		i;
+int		ft_isalnum(char c);
+int		ft_atoi(const char *str);
+int		ft_strlen(char *str);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
-	s2 = malloc(sizeof(char) * (ft_strlen((char *)s1) + 1));
-	i = 0;
-	while (s1[i])
-	{
-		s2[i] = s1[i];
-		i++;
-	}
-	s2[i] = '\0';
-	return (s2);
-}
-/*#include <stdio.h>
-#include <string.h>
-int main()
-{
-	const char s[] = "hello";
 
-	printf("%s \n",ft_strdup(s));
-	printf("%s \n",strdup(s));
-	printf("%d \n",strdup(s)[5]);
-	
-	return(0);
-}*/
+#endif
